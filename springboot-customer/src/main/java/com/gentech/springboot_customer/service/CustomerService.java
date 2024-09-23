62ceda7c34e2e76d480ce2c9046a0a34b90ac945
Vijay <arumugamg21068@gmail.com>
2024-09-23 18:40:48 +0530

    Deleted File

--- springboot-customer/src/main/java/com/gentech/springboot_customer/service/CustomerService.java
+++
@@ -1,9 +0,0 @@
-package com.gentech.springboot_customer.service;
-
-import com.gentech.springboot_customer.entity.CustomerEntity;
-
-import java.util.List;
-
-public interface CustomerService {
-    List<CustomerEntity> getCustomers();
-}
